8a4d05ae7379b320e218526403cf6d142bcbe8d3
SriRajeswari <shripriya996@gmail.com>
2015-03-04 00:58:10 +0530

    initial commit

---
+++ ProjectPink/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjectPink.Startup))]
+namespace ProjectPink
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
